264c8474b7509a4a2601356a84e122eae190a5b0
Benjamin Kluck <ben@fug.lu>
2018-11-18 12:53:04 +0100

    Acceptance tests

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -11,6 +11,8 @@ const init = async () => {
   process.env.restaurants_table = "restaurants-dev-fuglu";
   process.env.order_events_stream = "orders-dev-fuglu";
   process.env.restaurant_notification_topic = "restaurants-dev-fuglu";
+  process.env.TEST_ROOT =
+    "https://k863xwjjya.execute-api.eu-west-1.amazonaws.com/dev";
 
   initialized = true;
 };
