44734c3d078ec1b1d674bb42203f0b2957e165bc
Benjamin Kluck <ben@fug.lu>
2018-11-18 14:17:32 +0100

    SSM

--- seed-restaurants.js
+++ seed-restaurants.js
@@ -50,7 +50,12 @@ let restaurants = [
 ];
 
 const getTableName = async () => {
-  return `restaurants-${STAGE}-fuglu`;
+  console.log("getting table name...");
+  const req = {
+    Name: `/workshop-fuglu/${STAGE}/table_name`
+  };
+  const ssmResp = await ssm.getParameter(req).promise();
+  return ssmResp.Parameter.Value;
 };
 
 const run = async () => {
